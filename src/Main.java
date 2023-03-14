171f3bfce4e0f09db3d9ae5dd2745fdeaf8ad172
ChuprinaVlad <Chuprinavlad@ukr.net>
2023-03-14 23:37:12 +0200

    Створив анонімний клас

--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,15 @@ public class Main {
         Truck truck = new Truck(150, 9150, "red", new byte[] {10,0,10}, false);
         truck.engine.setValues(false,2000);
         truck.engine.info();
+
+            System.out.println("\n");
+
+        System.out.println("Info FlyCar:");
+        new Auto (860,3600, "blue", new byte[] {2,8,0}){
+            void flyCar() {
+                this.engine.info();
+                System.out.println("This is car FlyCar");
+            }
+        };
     }
 }
